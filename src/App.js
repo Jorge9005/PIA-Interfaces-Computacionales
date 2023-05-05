8b657270011fc0f53bc9c7ce61077eb28283d4f2
Jorge Sandoval <jorge9005@outlook.com>
2023-05-05 02:00:45 -0600

    Creación de la página de Inicio

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css';
 import Navbar from './components/navbar/Navbar'
+import Inicio from './components/inicio/Inicio';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <p className="text-warning">Hola</p>
+      <Inicio></Inicio>
     </div>
   );
 }

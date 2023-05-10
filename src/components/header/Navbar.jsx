5a595b5401a499452e094651cb0743e8d35c6135
Jorge Sandoval <jorge9005@outlook.com>
2023-05-09 23:13:16 -0600

    Cambio de título en el index.html

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar(props) {
     return (
         <div>
             <Link className='blog-header-logo' to={'/inicio'}>Inicio</Link>
-            <Link className='blog-header-logo' to={'/contacto'}>Contacto</Link>
             <Link className='blog-header-logo' to={'/productos'}>Productos</Link>
             <Link className='blog-header-logo' to={'/servicios'}>Servicios</Link>
+            <Link className='blog-header-logo' to={'/contacto'}>Contacto</Link>
         </div>
     );
 }

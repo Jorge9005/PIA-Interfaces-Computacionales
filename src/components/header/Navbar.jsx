0f50660cb4322a24642f806c1bfcb660be75732b
Jorge Sandoval <jorge9005@outlook.com>
2023-05-21 01:19:37 -0600

    Footer responsivo

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -4,39 +4,41 @@ import icono from '../../assets/img/icono.png';
 import './navbar.css';
 
 function Navbar() {
-    const [showLinks, setShowLinks] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
 
-    const toggleLinks = () => {
-        setShowLinks(!showLinks);
-    };
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
 
-    return (
-        <div>
-            <div className="wrapper-navbar">
-                <button className="toggle-button" onClick={toggleLinks}>
-                    Menu
-                </button>
-                <Link className='link-navbar' to={'/inicio'}><img src={icono} alt="logo" /></Link>
-                <div className={`links ${showLinks ? 'show' : ''}`}>
-                    <Link className='link-navbar' to={'/inicio'}>
-                        Inicio
-                    </Link>
-                    <Link className='link-navbar' to={'/productos'}>
-                        Productos
-                    </Link>
-                    <Link className='link-navbar' to={'/servicios'}>
-                        Servicios
-                    </Link>
-                    <Link className='link-navbar' to={'/contacto'}>
-                        Contacto
-                    </Link>
-                    <Link className='link-navbar' to={'/equipo'}>
-                        Equipo
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="wrapper-navbar">
+        <button className="toggle-button" onClick={toggleLinks}>
+          Menu
+        </button>
+        <Link className='link-navbar' to={'/inicio'}>
+          <img src={icono} alt="logo" />
+        </Link>
+        <div className={`links ${showLinks ? 'show' : ''}`}>
+          <Link className='link-navbar' to={'/inicio'}>
+            Inicio
+          </Link>
+          <Link className='link-navbar' to={'/productos'}>
+            Productos
+          </Link>
+          <Link className='link-navbar' to={'/servicios'}>
+            Servicios
+          </Link>
+          <Link className='link-navbar' to={'/contacto'}>
+            Contacto
+          </Link>
+          <Link className='link-navbar' to={'/equipo'}>
+            Equipo
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
